df3e2284ecab841c401f8705c842efbe0b985d98
Dumpinator <co.mannmail@gmail.com>
2017-04-05 14:18:44 +0100

    removed test code

--- Project 3 Windows form/Project 3 Windows form/Game.cs
+++ Project 3 Windows form/Project 3 Windows form/Game.cs
@@ -4,12 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Xna.Framework.Input;
 
 namespace Project_3_Windows_form
 {
     class Game
     {
-        FileStream stream = File.Open(content)
     }
 }
