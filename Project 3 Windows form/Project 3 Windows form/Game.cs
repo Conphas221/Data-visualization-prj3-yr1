f124bd5f383830a9b113e9a2395b211f34f5984c
Dumpinator <co.mannmail@gmail.com>
2017-04-05 12:06:38 +0100

    Added project to work in

---
+++ Project 3 Windows form/Project 3 Windows form/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project_3_Windows_form
+{
+    class Game
+    {
+        FileStream stream = File.Open(content)
+    }
+}
