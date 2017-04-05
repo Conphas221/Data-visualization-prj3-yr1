42ebb7cae2fff8ef71cbf824b5c9fc875bbfba7f
Dumpinator <co.mannmail@gmail.com>
2017-04-05 14:10:42 +0100

    Added first import

---
+++ Project 3 Windows form/Project 3 Windows form/Imports/A13Import.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//new usings
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Project_3_Windows_form.Imports
+{
+    class A13Import : Aimport
+    {
+            public List<int> ImportSpeeds(int search)
+            {
+                List<int> speed = new List<int>();
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd;
+                    string searchQuery = @"SELECT * FROM a13 WHERE snelheid LIKE @search";
+                    cmd = new MySqlCommand(searchQuery, conn);
+
+                    cmd.Parameters.AddWithValue("@search", "%" + search + "%");
+
+                    cmd.Prepare();
+
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        speed.Add(dataReader.GetInt32("snelheid"));
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Importing speed failed");
+                }
+                finally
+                {
+                    conn.Close();
+                }
+                return speed;
+            }
+        }
+    }
---
+++ Project 3 Windows form/Project 3 Windows form/Imports/Aimport.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Project_3_Windows_form.Imports
+{
+    abstract class Aimport
+    {
+        protected MySqlConnection conn;
+
+        public Aimport()
+        {
+            //Fill in the correct Database information
+            conn = new MySqlConnection("Uid=thomaeh144_CRM;Pwd=");
+        }
+        // Converts null values to useable values
+        public string DataReaderGetStringNull(string value, MySqlDataReader dataReader)
+        {
+            int ordinal = dataReader.GetOrdinal(value);
+            return dataReader.IsDBNull(ordinal) ? null : dataReader.GetString(ordinal);
+        }
+        public int DataReaderGetIntNull(string value, MySqlDataReader dataReader)
+        {
+
+            int ordinal = dataReader.GetOrdinal(value);
+            return dataReader.IsDBNull(ordinal) ? 0 : dataReader.GetInt32(ordinal);
+        }
+    }
+}
