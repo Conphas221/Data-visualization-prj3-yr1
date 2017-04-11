0dc84837eed1ace1b30dbd493a3a8715f6f9432b
MarcelvanderHarst <0928460@hr.nl>
2017-04-11 15:01:41 +0200

    Graph changes

--- Project 3 Windows form/Project 3 Windows form/Form1.Designer.cs
+++ Project 3 Windows form/Project 3 Windows form/Form1.Designer.cs
@@ -29,84 +29,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.BarChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.button1 = new System.Windows.Forms.Button();
             this.Result = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.spdButton = new System.Windows.Forms.Button();
+            this.btnMinToDay = new System.Windows.Forms.Button();
+            this.btnSpeedCar = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnTempCar = new System.Windows.Forms.Button();
+            this.btnSpeed = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.BarChart)).BeginInit();
             this.SuspendLayout();
             // 
             // BarChart
             // 
-            chartArea1.Name = "ChartArea2";
-            this.BarChart.ChartAreas.Add(chartArea1);
-            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
-            legend1.Name = "Legend1";
-            this.BarChart.Legends.Add(legend1);
-            this.BarChart.Location = new System.Drawing.Point(12, 12);
+            this.BarChart.BackColor = System.Drawing.Color.Transparent;
+            chartArea2.Name = "ChartArea2";
+            this.BarChart.ChartAreas.Add(chartArea2);
+            legend2.BackColor = System.Drawing.Color.Transparent;
+            legend2.DockedToChartArea = "ChartArea2";
+            legend2.IsDockedInsideChartArea = false;
+            legend2.Name = "Legend1";
+            this.BarChart.Legends.Add(legend2);
+            this.BarChart.Location = new System.Drawing.Point(16, 15);
+            this.BarChart.Margin = new System.Windows.Forms.Padding(4);
             this.BarChart.Name = "BarChart";
-            series1.ChartArea = "ChartArea2";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Legend = "Legend1";
-            series1.Name = "BarChart";
-            this.BarChart.Series.Add(series1);
-            this.BarChart.Size = new System.Drawing.Size(837, 332);
+            series2.ChartArea = "ChartArea2";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "BarChart";
+            this.BarChart.Series.Add(series2);
+            this.BarChart.Size = new System.Drawing.Size(1210, 454);
             this.BarChart.TabIndex = 0;
             this.BarChart.Text = "chart1";
             this.BarChart.Click += new System.EventHandler(this.chart1_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(855, 386);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(161, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Count all speeds";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Result
             // 
             this.Result.AutoSize = true;
-            this.Result.Location = new System.Drawing.Point(439, 112);
+            this.Result.Location = new System.Drawing.Point(585, 138);
+            this.Result.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Result.Name = "Result";
-            this.Result.Size = new System.Drawing.Size(0, 13);
+            this.Result.Size = new System.Drawing.Size(0, 17);
             this.Result.TabIndex = 2;
             // 
-            // button2
+            // btnMinToDay
             // 
-            this.button2.Location = new System.Drawing.Point(688, 386);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(161, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Convert minutes to days";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnMinToDay.Location = new System.Drawing.Point(1261, 32);
+            this.btnMinToDay.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMinToDay.Name = "btnMinToDay";
+            this.btnMinToDay.Size = new System.Drawing.Size(70, 26);
+            this.btnMinToDay.TabIndex = 3;
+            this.btnMinToDay.Text = "Convert minutes to days";
+            this.btnMinToDay.UseVisualStyleBackColor = true;
+            this.btnMinToDay.Click += new System.EventHandler(this.btnMinToDay_Click);
             // 
-            // spdButton
+            // btnSpeedCar
             // 
-            this.spdButton.Location = new System.Drawing.Point(855, 357);
-            this.spdButton.Name = "spdButton";
-            this.spdButton.Size = new System.Drawing.Size(161, 23);
-            this.spdButton.TabIndex = 4;
-            this.spdButton.Text = "Show average speed/cars";
-            this.spdButton.UseVisualStyleBackColor = true;
-            this.spdButton.Click += new System.EventHandler(this.spdButton_Click);
+            this.btnSpeedCar.Location = new System.Drawing.Point(1011, 477);
+            this.btnSpeedCar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSpeedCar.Name = "btnSpeedCar";
+            this.btnSpeedCar.Size = new System.Drawing.Size(215, 28);
+            this.btnSpeedCar.TabIndex = 4;
+            this.btnSpeedCar.Text = "Show average speed/cars";
+            this.btnSpeedCar.UseVisualStyleBackColor = true;
+            this.btnSpeedCar.Click += new System.EventHandler(this.btnSpeedCar_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(16, 483);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(109, 483);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnTempCar
+            // 
+            this.btnTempCar.Location = new System.Drawing.Point(828, 477);
+            this.btnTempCar.Name = "btnTempCar";
+            this.btnTempCar.Size = new System.Drawing.Size(176, 28);
+            this.btnTempCar.TabIndex = 7;
+            this.btnTempCar.Text = "Show average temp/cars";
+            this.btnTempCar.UseVisualStyleBackColor = true;
+            this.btnTempCar.Click += new System.EventHandler(this.btnTempCar_Click);
+            // 
+            // btnSpeed
+            // 
+            this.btnSpeed.Location = new System.Drawing.Point(584, 476);
+            this.btnSpeed.Name = "btnSpeed";
+            this.btnSpeed.Size = new System.Drawing.Size(238, 28);
+            this.btnSpeed.TabIndex = 8;
+            this.btnSpeed.Text = "Show average speed at 2 locations";
+            this.btnSpeed.UseVisualStyleBackColor = true;
+            this.btnSpeed.Click += new System.EventHandler(this.btnSpeed_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1019, 421);
-            this.Controls.Add(this.spdButton);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1359, 518);
+            this.Controls.Add(this.btnSpeed);
+            this.Controls.Add(this.btnTempCar);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSpeedCar);
+            this.Controls.Add(this.btnMinToDay);
             this.Controls.Add(this.Result);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.BarChart);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.BarChart)).EndInit();
@@ -118,10 +162,13 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart BarChart;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label Result;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button spdButton;
+        private System.Windows.Forms.Button btnMinToDay;
+        private System.Windows.Forms.Button btnSpeedCar;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnTempCar;
+        private System.Windows.Forms.Button btnSpeed;
     }
 }
 
